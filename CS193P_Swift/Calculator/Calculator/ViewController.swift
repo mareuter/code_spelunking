3b5d29f34fd895f554d30eb1711b122d72729027
Michael Reuter <mareuternh@gmail.com>
2015-05-11 23:04:08 -0400

    Checkpointing Calculator work.

---
+++ CS193P_Swift/Calculator/Calculator/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Michael Reuter on 5/11/15.
+//  Copyright (c) 2015 Reuter Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    @IBOutlet weak var display: UILabel!
+
+}
+
---
+++ CS193P_Swift/README.md
@@ -0,0 +1,1 @@
+This directory contains the homework projects for Stanford University's CS193P class on the Swift programming language.
