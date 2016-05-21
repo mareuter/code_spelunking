847a95febdbf2d364f9ab8e115b21a5b89cd238d
Michael Reuter <mareuternh@gmail.com>
2016-05-20 18:28:34 -0700

    Adding Psychologist demo.

---
+++ CS193P_Swift/Psychologist/Psychologist/HappinessViewController.swift
@@ -0,0 +1,54 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Michael Reuter on 5/12/16.
+//  Copyright © 2016 Reuter Inc. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController, FaceViewDataSource
+{
+    @IBOutlet weak var faceView: FaceView! {
+        didSet {
+            faceView.dataSource = self
+            faceView.addGestureRecognizer(UIPinchGestureRecognizer(target: faceView, action: "scale:"))
+        }
+    }
+
+    private struct Constants {
+        static let HappinessGestureScale: CGFloat = 4
+    }
+    
+    var happiness: Int = 100 { // 0 = very sad, 100 = ecstatic
+        didSet {
+            happiness = min(max(happiness, 0), 100)
+            print("happiness = \(happiness)")
+            updateUI()
+        }
+    }
+    @IBAction func changeHappiness(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .Ended: fallthrough
+        case .Changed:
+            let translation = gesture.translationInView(faceView)
+            let happinessChange = -Int(translation.y / Constants.HappinessGestureScale)
+            if happinessChange != 0 {
+                happiness += happinessChange
+                gesture.setTranslation(CGPointZero, inView: faceView)
+            }
+        default: break
+        }
+    }
+
+    private func updateUI()
+    {
+        faceView?.setNeedsDisplay()
+        title = "\(happiness)"
+    }
+    
+    func smilinessForFaceView(sender: FaceView) -> Double? {
+        return Double(happiness - 50) / 50
+    }
+}
---
+++ CS193P_Swift/Psychologist/Psychologist/TextViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TextViewController.swift
+//  Psychologist
+//
+//  Created by Michael Reuter on 5/19/16.
+//  Copyright © 2016 Reuter Inc. All rights reserved.
+//
+
+import UIKit
+
+class TextViewController: UIViewController
+{
+    @IBOutlet weak var textView: UITextView! {
+        didSet {
+            textView.text = text
+        }
+    }
+    
+    var text: String = "" {
+        didSet {
+            textView?.text = text
+        }
+    }
+    
+    override var preferredContentSize: CGSize {
+        get {
+            if textView != nil && presentingViewController != nil {
+                return textView.sizeThatFits(presentingViewController!.view.bounds.size)
+            } else {
+                return super.preferredContentSize
+            }
+        }
+        set {
+            super.preferredContentSize = newValue
+        }
+    }
+}
