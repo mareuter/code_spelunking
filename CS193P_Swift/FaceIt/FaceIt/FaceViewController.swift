f7ac35dc18da84e93054ec6f6f39eabb8c51e394
Michael Reuter <mareuternh@gmail.com>
2016-05-28 09:57:56 -0700

    FaceIt demo after Lecture 5.

---
+++ CS193P_Swift/FaceIt/FaceIt/FaceViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Michael Reuter on 5/26/16.
+//  Copyright © 2016 Reuter Inc. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController
+{
+    var expression = FacialExpression(eyes: .Open, eyeBrows: .Normal, mouth: .Smile) {
+        didSet {
+            updateUI()
+        }
+    }
+
+    @IBOutlet weak var faceView: FaceView! {
+        didSet {
+            faceView.addGestureRecognizer(UIPinchGestureRecognizer(target: faceView, action: #selector(FaceView.changeScale(_:))))
+            
+            let happierSwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(FaceViewController.increaseHappiness))
+            happierSwipeGestureRecognizer.direction = .Up
+            faceView.addGestureRecognizer(happierSwipeGestureRecognizer)
+            
+            let sadderSwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(FaceViewController.decreaseHappiness))
+            sadderSwipeGestureRecognizer.direction = .Down
+            faceView.addGestureRecognizer(sadderSwipeGestureRecognizer)
+            
+            faceView.addGestureRecognizer(UIRotationGestureRecognizer(target: self, action: #selector(FaceViewController.changeBrows(_:))))
+            
+            updateUI()
+        }
+    }
+    @IBAction func toggleEyes(recognizer: UITapGestureRecognizer) {
+        if recognizer.state == .Ended {
+            switch expression.eyes {
+            case .Open:
+                expression.eyes = .Closed
+            case .Closed:
+                expression.eyes = .Open
+            case .Squinting:
+                break
+            }
+        }
+    }
+    
+    func changeBrows(recognizer: UIRotationGestureRecognizer) {
+        // Want incremental rotation
+        switch recognizer.state {
+        case .Changed,.Ended:
+            if recognizer.rotation > CGFloat(M_PI/4) {
+                expression.eyeBrows = expression.eyeBrows.moreRelaxedBrow()
+                recognizer.rotation = 0.0
+            } else if recognizer.rotation < -CGFloat(M_PI/4) {
+                expression.eyeBrows = expression.eyeBrows.moreFurrowedBrow()
+                recognizer.rotation = 0.0
+            }
+        default:
+            break
+        }
+    }
+    
+    func increaseHappiness() {
+        expression.mouth = expression.mouth.happierMouth()
+    }
+    
+    func decreaseHappiness() {
+        expression.mouth = expression.mouth.sadderMouth()
+    }
+    
+    private var mouthCurvatures = [FacialExpression.Mouth.Frown:-1.0,.Grin:0.5,.Smirk:-0.5,.Neutral:0.0,.Smile:1.0]
+    
+    private var eyeBrowTilts = [FacialExpression.EyeBrows.Relaxed:0.5,.Furrowed:-0.5,.Normal:0.0]
+    
+    private func updateUI() {
+        switch expression.eyes {
+        case .Open: faceView.eyesOpen = true
+        case .Closed: faceView.eyesOpen = false
+        case .Squinting: faceView.eyesOpen = false
+        }
+        
+        faceView.mouthCurvature = mouthCurvatures[expression.mouth] ?? 0.0
+        faceView.eyeBrowTilt = eyeBrowTilts[expression.eyeBrows] ?? 0.0
+    }
+
+}
+
