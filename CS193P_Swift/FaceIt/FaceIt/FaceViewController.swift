c1be5dbf8a51d52aa7e36e1f10457a337efd3b42
Michael Reuter <mareuternh@gmail.com>
2016-05-28 20:29:28 -0700

    Changes to FaceIt after Lecture 6.

--- CS193P_Swift/FaceIt/FaceIt/FaceViewController.swift
+++ CS193P_Swift/FaceIt/FaceIt/FaceViewController.swift
@@ -75,14 +75,16 @@ class FaceViewController: UIViewController
     private var eyeBrowTilts = [FacialExpression.EyeBrows.Relaxed:0.5,.Furrowed:-0.5,.Normal:0.0]
     
     private func updateUI() {
-        switch expression.eyes {
-        case .Open: faceView.eyesOpen = true
-        case .Closed: faceView.eyesOpen = false
-        case .Squinting: faceView.eyesOpen = false
+        if faceView != nil {
+            switch expression.eyes {
+            case .Open: faceView.eyesOpen = true
+            case .Closed: faceView.eyesOpen = false
+            case .Squinting: faceView.eyesOpen = false
+            }
+            
+            faceView.mouthCurvature = mouthCurvatures[expression.mouth] ?? 0.0
+            faceView.eyeBrowTilt = eyeBrowTilts[expression.eyeBrows] ?? 0.0
         }
-        
-        faceView.mouthCurvature = mouthCurvatures[expression.mouth] ?? 0.0
-        faceView.eyeBrowTilt = eyeBrowTilts[expression.eyeBrows] ?? 0.0
     }
 
 }
