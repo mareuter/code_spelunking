468d3948c36eed08fce9b44ee4af79e7ca82d896
Michael Reuter <mareuternh@gmail.com>
2016-06-09 19:11:07 -0700

    Cassini Demo after Lecture 7.

---
+++ CS193P_Swift/Cassini/ImageViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ImageViewController.swift
+//  Cassini
+//
+//  Created by Michael Reuter on 6/9/16.
+//  Copyright © 2016 Reuter Inc. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController
+{
+    var imageURL: NSURL? {
+        didSet {
+            image = nil
+            fetchImage()
+        }
+    }
+    
+    private func fetchImage() {
+        if let url = imageURL {
+            if let imageData = NSData(contentsOfURL: url) {
+                image = UIImage(data: imageData)
+            }
+        }
+    }
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    {
+        didSet {
+            scrollView.contentSize = imageView.frame.size
+        }
+    }
+    
+    private var imageView = UIImageView()
+    
+    private var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        set {
+            imageView.image = newValue
+            imageView.sizeToFit()
+            scrollView?.contentSize = imageView.frame.size
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.addSubview(imageView)
+        imageURL = NSURL(string: DemoURL.Stanford)
+    }
+
+
+}
