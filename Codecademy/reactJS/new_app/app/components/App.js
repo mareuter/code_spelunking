139bfeac162fade30602c49d806c7d2d5194d54e
Michael Reuter <mareuternh@gmail.com>
2017-06-22 14:49:52 -0700

    Example app setup.

---
+++ Codecademy/reactJS/new_app/app/components/App.js
@@ -0,0 +1,9 @@
+var React = require('react')
+
+var MyComponent = React.createClass({
+  render: function() {
+    return <h1>Hello Everyone!</h1>
+  }
+});
+
+module.exports = MyComponent;
---
+++ Codecademy/reactJS/new_app/webpack.config.js
@@ -0,0 +1,24 @@
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body' /* HTML tag for insertion point */
+});
+
+module.exports = {
+  entry: __dirname + '/app/index.js',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  output: {
+    filename: 'transformed.js',
+    path: __dirname + '/build'
+  },
+  plugins: [HTMLWebpackPluginConfig]
+};
